2b5154554d9b841ed6c08aa203e4ffd9a8ba379a
JTLiner925 <[jtliner925@gmail.com]>
2020-03-18 10:29:52 -0500

    adding correct content, some videos are not working

--- index.js
+++ index.js
@@ -7,13 +7,111 @@ const API_URL = 'https://api.esv.org/v3/passage/text/';
 // Authorization: Token ccbf0a4b9f501f9c1e71052941583c219e9c85ce
 
 const STORE ={
+  stories: [
+    {
+      title: 'Sinful Woman',
+      scripture: 'Luke+7:36-50',
+      questions: [ 
+        'What grabbed your attention?',
+        'What did you like in the passage?',
+        'What bothered you in the passage?',
+        'What does this say about God?',
+        'What does this say about humankind?',
+        'What does this say about the life God wants us to live?'
+      ],
+      resources: ['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'] 
+    },
+    {
+      title: 'Who can come to God',
+      scripture: 'Luke+18:9-17',
+      questions: [ 
+        'What grabbed your attention?',
+        'What did you like in the passage?',
+        'What bothered you in the passage?',
+        'What does this say about God?',
+        'What does this say about humankind?',
+        'What does this say about the life God wants us to live?'
+      ],
+      resources: ['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'] 
+    },
+    {
+      title: 'Forgiveness',
+      scripture: 'Matthew 18:21-35',
+      questions: [ 
+        'What grabbed your attention?',
+        'What did you like in the passage?',
+        'What bothered you in the passage?',
+        'What does this say about God?',
+        'What does this say about humankind?',
+        'What does this say about the life God wants us to live?'
+      ],
+      resources: ['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'] 
+    },
+    {
+      title: 'Merciful Father',
+      scripture: 'Luke+15:11-32',
+      questions: [ 
+        'What grabbed your attention?',
+        'What did you like in the passage?',
+        'What bothered you in the passage?',
+        'What does this say about God?',
+        'What does this say about humankind?',
+        'What does this say about the life God wants us to live?'
+      ],
+      resources: ['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'] 
+    },
+    {
+      title: 'Who is Jesus',
+      scripture: 'Mark+2:1-12',
+      questions: [ 
+        'What grabbed your attention?',
+        'What did you like in the passage?',
+        'What bothered you in the passage?',
+        'What does this say about God?',
+        'What does this say about humankind?',
+        'What does this say about the life God wants us to live?'
+      ],
+      resources: ['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'] 
+    },
+    {
+      title: 'Crucifixion',
+      scripture: 'Luke+23:32-50',
+      questions: [ 
+        'What grabbed your attention?',
+        'What did you like in the passage?',
+        'What bothered you in the passage?',
+        'What does this say about God?',
+        'What does this say about humankind?',
+        'What does this say about the life God wants us to live?'
+      ],
+      resources: ['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'] 
+    },
+    {
+      title: 'Born Again',
+      scripture: 'John+3:1-21',
+      questions: [ 
+        'What grabbed your attention?',
+        'What did you like in the passage?',
+        'What bothered you in the passage?',
+        'What does this say about God?',
+        'What does this say about humankind?',
+        'What does this say about the life God wants us to live?'
+      ],
+      resources: ['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'] 
+    }
 
-  1:['oZcEY6qPV84','oZcEY6qPV84','oZcEY6qPV84'],
-  2:['Ej0WNwYGKHg','Ej0WNwYGKHg','Ej0WNwYGKHg'],
-  3:['Ej0WNwYGKHg','oZcEY6qPV84','oZcEY6qPV84'],
-  4:['oZcEY6qPV84','Ej0WNwYGKHg','Ej0WNwYGKHg'],
+  ],
+  1:['YihKbG8-X3U','bETuP57T_90','e7iKmka651U','oZcEY6qPV84'],
+  2:['OpfuKKH_SCE','N9w6284vC1w','huTo3G248S8','oZcEY6qPV84'],
+  3:['7O3prdbRpAk','zPQKj2Zptr0','9f2FXxDVO6w','oZcEY6qPV84'],
+  4:['2emelR7lGmw','36zYEgcS_Z8','kHeucaCZcKQ','oZcEY6qPV84'],
+  5:['_H62gNjAYOs','LqBpifDpNKc','riO7nzyqnSw','oZcEY6qPV84'],
+  6:['ASYyTebCods','GFLk6v7US3I','Fvb6o50A5j4','oZcEY6qPV84'],
+  7:['leh-4fCc5MI','yOzf0VrDNGU','9f2FXxDVO6w','oZcEY6qPV84'],
 
-}
+};
+
+ 
 
 function formatQueryParams(params){
   const queryItems = Object.keys(params)
@@ -55,6 +153,7 @@ function displayVerses(resJson){
   // for(let i = 0; i< resJson.length; i++){
   $('#bible-verses').append(
     `<li><h3>${resJson.canonical}</h3>
+    <h4>Read passage 2X / Rebuild it.</h4>
       <p>${resJson.passages[0]}</p></li>`
       
   );
@@ -69,8 +168,8 @@ function displayResults(responseJson){
   $('#results-list').empty();
   for(let i = 0; i< responseJson.length; i++){
     $('#results-list').append(
-      `<li><h3>By ${responseJson[i].items[0].snippet.channelTitle}</h3>
-      <p>${responseJson[i].items[0].snippet.description}</p>
+      `<li><h3>By ${responseJson[i].items[0].snippet.title}</h3>
+      
       <iframe id="thumbnail"
     title="${responseJson[i].items[0].snippet.title}"
     width="480"
@@ -79,7 +178,7 @@ function displayResults(responseJson){
 </iframe>
       </li>`
     );}
-    $('#questions-section').removeClass('hidden');
+  $('#questions-section').removeClass('hidden');
   $('#results').removeClass('hidden');
 }
 
@@ -90,14 +189,14 @@ function getVideo(lesson){
   // Accept: application/json
   Promise.all(STORE[lesson].map(id => {
     return fetch(`https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${key}&part=snippet,contentDetails,statistics,status`)
-    .then(response => { 
+      .then(response => { 
       
-      return response.json();
-    })
-    .then(responseJson => responseJson)
-    .catch(err => { $('#js-error-message')
-      .text(`Something went wrong: ${err.message}`);
-    });
+        return response.json();
+      })
+      .then(responseJson => responseJson)
+      .catch(err => { $('#js-error-message')
+        .text(`Something went wrong: ${err.message}`);
+      });
   })).then(responseJson => {
     console.log(responseJson);
     return displayResults(responseJson);});
@@ -110,6 +209,7 @@ function watchForm(){
   $('#lesson-select').change(event => {
     event.preventDefault();
     // const searchTerm = $('').val();
+    youTubeVid = STORE.stories.resources;
     youTubeId = $('#lesson-select option:selected').attr('ytid');
     lessonSelect = $('#lesson-select option:selected').val();
     getVerses(lessonSelect);
